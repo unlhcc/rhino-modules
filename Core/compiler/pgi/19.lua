f6b7ce8c97a37416849dc76854ccf35ceab6f3fd
Adam Caprez <acaprez2@unl.edu>
2019-06-05 23:59:07 +0000

    Add module file for PGI 19.

---
+++ Core/compiler/pgi/19.lua
@@ -0,0 +1,44 @@
+local help_message = [[
+PGI Compilers 19
+
+This module loads PGI Compiler variables.
+The command directory is added to PATH.
+The library directory is added to LD_LIBRARY_PATH.
+The include directory is added to INCLUDE.
+The man     directory is added to MANPATH.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: PGI Compilers")
+whatis("Version: 19")
+whatis("Category: compiler")
+whatis("Keywords: System, compiler")
+whatis("URL: http://www.pgroup.com")
+
+prepend_path("PATH",                "/util/comp/pgi/2019/linux86-64/2019/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/pgi/2019/linux86-64/2019/lib")
+prepend_path("MANPATH",             "/util/comp/pgi/2019/linux86-64/2019/man")
+prepend_path("INCLUDE",             "/util/comp/pgi/2019/linux86-64/2019/include")
+
+local mroot = os.getenv("MODULEPATH_ROOT") or "/util/opt/modulefiles"
+local mdir = pathJoin(mroot,"Compiler","pgi","19")
+prepend_path("MODULEPATH",          mdir)
+
+setenv("CC",	"pgcc")
+setenv("FC",	"pgf90")
+setenv("F90",	"pgf90")
+setenv("F77",	"pgf77")
+setenv("CPP",	"pgcc -E")
+setenv("CXX",	"pgc++")
+
+setenv("CFLAGS",        "-tp=bulldozer")
+setenv("FFLAGS",        "-tp=bulldozer")
+setenv("CXXFLAGS",        "-tp=bulldozer")
+setenv("F90FLAGS",        "-tp=bulldozer")
+setenv("FCFLAGS",        "-tp=bulldozer")
+
+family("compiler")
+
+prepend_path("LIBRARY_PATH",     "/util/comp/pgi/2019/linux86-64/2019/lib")
+prepend_path("LIBRARY_PATH",     "/util/comp/pgi/2019/linux86-64/2019/lib")
