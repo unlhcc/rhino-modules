81633e14f37f7f99cf47a37061d2c3cfbfb00cc1
Adam Caprez <acaprez2@unl.edu>
2019-06-13 23:53:02 +0000

    Add default modules.

---
+++ Core/StdEnv.lua
@@ -0,0 +1,9 @@
+local help_message = [[
+This module provide the default software packages for
+Rhino.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: Rhino Default Modules")
+load("compiler/gcc/9.1","openmpi/4.0","R/3.5","python/3.7","perl/5.26","git/2.22","cmake/3.14","automake/1.16","matlab/r2019a")
