1b9e5f351eef7b1feecc55af2d3201e9bbddd7f4
Jingchao Zhang <jzhang30@unl.edu>
2019-06-15 22:18:34 +0000

    Lammps 05 jun2019

---
+++ MPI/gcc/9.1/openmpi/4.0/lammps/05Jun2019.lua
@@ -0,0 +1,21 @@
+local help_message = [[
+LAMMPS 06/05/2016
+
+This module loads the lammps environment, built with the GCC
+compiler.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: LAMMPS")
+whatis("Version: 05Jun2019")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://lammps.sandia.gov/")
+
+setenv("LAMMPS_POTENTIALS",         "/util/opt/lammps/potentials")
+prepend_path("PATH",                "/util/opt/lammps/05Jun2019/gcc/9.1/openmpi/4.0/bin")
+
+family("lammps")
+
