9439add11e52fd5b47e9113e84727b8c0000c3e6
Jingchao Zhang <jzhang30@unl.edu>
2019-06-27 19:44:56 +0000

    fftw3 3.3.8 with intel 19 and ompi 4.0

---
+++ MPI/intel/19.0/openmpi/4.0/fftw3/3.3.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+fftw3 3.3
+
+This module loads the fftw3 environment, built with the Intel
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: fftw3")
+whatis("Version: 3.3")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.fftw.org/index.html")
+
+prepend_path("PATH",                "/util/opt/fftw3/3.3/intel/19/openmpi/4.0/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/fftw3/3.3/intel/19/openmpi/4.0/lib")
+prepend_path("LIBRARY_PATH",        "/util/opt/fftw3/3.3/intel/19/openmpi/4.0/lib")
+prepend_path("MANPATH",             "/util/opt/fftw3/3.3/intel/19/openmpi/4.0/share/man")
+setenv("FFTW3_INCLUDE",             "/util/opt/fftw3/3.3/intel/19/openmpi/4.0/include")
+prepend_path("CPATH",               "/util/opt/fftw3/3.3/intel/19/openmpi/4.0/include")
+family("fftw3")
+
