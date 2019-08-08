d87dfc0d13fa096e8c62822e925cffa7623434b9
Adam Caprez <acaprez2@unl.edu>
2019-08-08 20:16:19 +0000

    Add JASPERLIB and JASPERINC vars to jasper modules.

--- Compiler/intel/19/jasper/2.0.lua
+++ Compiler/intel/19/jasper/2.0.lua
@@ -20,4 +20,5 @@ prepend_path("INCLUDE",             "/util/opt/jasper/2.0/intel/19/include")
 prepend_path("CPATH",               "/util/opt/jasper/2.0/intel/19/include")
 prepend_path("LIBRARY_PATH",        "/util/opt/jasper/2.0/intel/19/lib64")
 prepend_path("PKG_CONFIG_PATH",     "/util/opt/jasper/2.0/intel/19/lib64/pkgconfig")
-
+setenv("JASPERINC","/util/opt/jasper/2.0/intel/19/include")
+setenv("JASPERLIB","/util/opt/jasper/2.0/intel/19/lib64")
