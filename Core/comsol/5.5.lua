49d27c03a37aa033cad9cd58fc23094ac50a12fd
Adam Caprez <acaprez2@unl.edu>
2020-05-21 00:55:41 +0000

    Add module file for COMSOL 5.5.

---
+++ Core/comsol/5.5.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads COMSOL.
+
+Version 5.5
+]]
+)
+
+whatis("Name: COMSOL")
+whatis("Version: 5.5")
+whatis("Category: application, simulation")
+whatis("Keywords: application, simulation")
+whatis("URL: https://www.comsol.com")
+whatis("Description: COMSOL Multiphysics")
+
+prepend_path("PATH",              "/util/opt/comsol/5.5/comsol55/multiphysics/bin/")
+
+local work=os.getenv("WORK")
+prepend_path("_JAVA_OPTIONS", "-Duser.home=" .. work, " ")
