e5ef166afd4baf067eb2487e08e7071d1a3af832
Adam Caprez <acaprez2@unl.edu>
2021-05-06 03:56:31 +0000

    Add OMPI 1.10 w/Intel 16 (hidden).

---
+++ Compiler/intel/16/openmpi/1.10.lua
@@ -0,0 +1,35 @@
+local help_message = [[
+OpenMPI Compilers 1.10
+
+This module loads the OpenMPI environment, built with the GNU
+compilers.  By loading this module, the following commands
+will be automatically available for compiling MPI applications:
+
+mpif77          (F77 source)
+mpif90          (F90 source)
+mpicc           (C source)
+mpiCC/mpicxx    (C++ source)
+]]
+
+help(help_message,"\n")
+
+whatis("Name: OpenMPI")
+whatis("Version: 1.10")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.open-mpi.org")
+
+prepend_path("PATH",                "/util/opt/openmpi/1.10/intel/16/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/openmpi/1.10/intel/16/lib")
+prepend_path("MANPATH",             "/util/opt/openmpi/1.10/intel/16/share/man")
+prepend_path("INCLUDE",             "/util/opt/openmpi/1.10/intel/16/include")
+prepend_path("CPATH",               "/util/opt/openmpi/1.10/intel/16/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/openmpi/1.10/intel/16/lib")
+prepend_path("PKG_CONFIG_PATH",     "/util/opt/openmpi/1.10/intel/16/lib/pkgconfig")
+
+local mroot = os.getenv("MODULEPATH_ROOT") or "/util/opt/modulefiles"
+local mdir = pathJoin(mroot,"MPI","intel","16","openmpi","1.10")
+prepend_path("MODULEPATH",          mdir)
+
+family("mpi")
+
