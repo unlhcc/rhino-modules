870d828723cdc100b32cadc75c2dc1477333f1a1
Mohammed Tanash <tanash@login.rhino.hcc.unl.edu>
2021-05-20 17:49:58 -0500

    Add module for Plumed 2.7.1 built with OMPI 4.0 and GCC 9.1

---
+++ MPI/gcc/9.1/openmpi/4.0/plumed/2.7.lua
@@ -0,0 +1,20 @@
+help (
+[[
+This module loads PLUMED with GCC-9.1 and OpenMPI-4.0
+Version 2.7.1
+]])
+
+whatis("Name: PLUMED")
+whatis("Version: 2.7.1")
+whatis("Category: molecular dynamics, plugin")
+whatis("Keywords: molecular dynamics, free-energy, sampling")
+whatis("Description: The community-developed PLUgin for MolEcular Dynamics.")
+whatis("URL: https://www.plumed.org/")
+
+prepend_path("PATH", "/util/opt/plumed/2.7.1/gcc/9.1/openmpi/4.0/bin")
+prepend_path("LD_LIBRARY_PATH", "/util/opt/plumed/2.7.1/gcc/9.1/openmpi/4.0/lib")
+prepend_path("INCLUDE", "/util/opt/plumed/2.7.1/gcc/9.1/openmpi/4.0/include")
+prepend_path("LIBRARY_PATH", "/util/opt/plumed/2.7.1/gcc/9.1/openmpi/4.0/lib")
+prepend_path("CPATH", "/util/opt/plumed/2.7.1/gcc/9.1/openmpi/4.0/include")
+
+family("plumed")
