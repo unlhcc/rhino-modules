3ef73ffa089fbb95651eee705147718763373beb
Adam Caprez <acaprez2@unl.edu>
2021-05-24 21:37:34 +0000

    Add module file for GMP 6.2.

---
+++ Compiler/pgi/19/gmp/6.2.lua
@@ -0,0 +1,27 @@
+local help_message = [[
+gmp 6.2
+
+This module loads GMP, built with the PGI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: gmp")
+whatis("Version: 6.2.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("Description: GNU MP is a library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating point numbers.")
+whatis("URL: https://gmplib.org")
+
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/gmp/6.2/pgi/19/lib")
+prepend_path("INCLUDE",             "/util/opt/gmp/6.2/pgi/19/include")
+
+family("gmp")
+
+prepend_path("CPATH",             "/util/opt/gmp/6.2/pgi/19/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/gmp/6.2/pgi/19/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/gmp/6.2/pgi/19/lib/pkgconfig")
+
+setenv("GMP", "/util/opt/gmp/6.2/pgi/19")
