4efd8b6d4f417aeaaeba5a333641f91c26ca4460
Adam Caprez <acaprez2@unl.edu>
2021-10-28 16:28:21 -0500

    Change to single-version naming.
    http://blog.llvm.org/2016/12/llvms-new-versioning-scheme.html

--- Core/compiler/clang/13.lua
+++ Core/compiler/clang/13.lua
@@ -1,5 +1,5 @@
 local help_message = [[
-Clang/LLVM Compilers 13.0
+Clang/LLVM Compilers 13
 
 This module loads Clang/LLVM Compiler variables.
 The command directory is added to PATH.
@@ -11,26 +11,26 @@ The man     directory is added to MANPATH.
 help(help_message,"\n")
 
 whatis("Name: Clang Compilers")
-whatis("Version: 13.0")
+whatis("Version: 13")
 whatis("Category: compiler")
 whatis("Keywords: System, compiler")
 whatis("URL: https://clang.llvm.org/index.html")
 
-prepend_path("PATH",                "/util/comp/clang/13.0/bin")
-prepend_path("LD_LIBRARY_PATH",     "/util/comp/clang/13.0/lib")
-prepend_path("LIBRARY_PATH",     "/util/comp/clang/13.0/lib")
-prepend_path("MANPATH",             "/util/comp/clang/13.0/share/man")
-prepend_path("INCLUDE",             "/util/comp/clang/13.0/include")
-setenv("CLANG", "/util/comp/clang/13.0")
+prepend_path("PATH",                "/util/comp/clang/13/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/clang/13/lib")
+prepend_path("LIBRARY_PATH",     "/util/comp/clang/13/lib")
+prepend_path("MANPATH",             "/util/comp/clang/13/share/man")
+prepend_path("INCLUDE",             "/util/comp/clang/13/include")
+setenv("CLANG", "/util/comp/clang/13")
 
 local mroot = os.getenv("MODULEPATH_ROOT") or "/util/opt/modulefiles"
-local mdir = pathJoin(mroot,"Compiler","clang","13.0")
+local mdir = pathJoin(mroot,"Compiler","clang","13")
 prepend_path("MODULEPATH",          mdir)
 
 setenv("CC",	"clang")
 setenv("CXX",	"clang++")
 
-append_path("CFLAGS","-march=sandybridge", " ")
-append_path("CXXFLAGS","-march=sandybridge", " ")
+append_path("CFLAGS","-march=bdver1", " ")
+append_path("CXXFLAGS","-march=bdver1", " ")
 
 family("compiler")
